18197a1a9a78805b8647cd14b8f4b4f80b4d3fac
nikitastukalov <nikita.stukalov07@gmail.com>
2022-10-25 13:23:30 +0300

    add saving images

--- app/src/main/java/com/nikovodi/sedatefarmer/view/fragments/GifViewFragment.kt
+++ app/src/main/java/com/nikovodi/sedatefarmer/view/fragments/GifViewFragment.kt
@@ -1,6 +1,10 @@
 package com.nikovodi.sedatefarmer.view.fragments
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +13,12 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.nikovodi.sedatefarmer.databinding.FragmentGifViewBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class GifViewFragment : Fragment() {
 
@@ -25,7 +35,7 @@ class GifViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: GifViewFragmentArgs by navArgs()
-        Glide.with(requireActivity()).load(args.url).into(binding.fullViewImage)
+        Glide.with(requireContext()).load(args.url).into(binding.fullViewImage)
         binding.fullViewTitle.text = "Title: ${args.title}"
         binding.fullViewUsername.text = "Author: ${args.username}"
         binding.fullViewRating.text = "Rating: ${args.rating}"
@@ -33,6 +43,54 @@ class GifViewFragment : Fragment() {
         binding.buttonBack.setOnClickListener {
             findNavController()
                 .navigate(GifViewFragmentDirections.actionGifViewFragmentToGifListFragment())
+        }
+
+        binding.buttonSave.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                saveImage(Glide.with(requireContext())
+                    .asBitmap()
+                    .load(args.url) // sample image
+                    .placeholder(android.R.drawable.progress_indeterminate_horizontal) // need placeholder to avoid issue like glide annotations
+                    .error(android.R.drawable.stat_notify_error) // need error to avoid issue like glide annotations
+                    .submit()
+                    .get())
+            }
+        }
+    }
+
+    private fun saveImage(image: Bitmap): String? {
+        var savedImagePath: String? = null
+        val imageFileName = "JPEG_" + "${System.currentTimeMillis()}" + ".jpg"
+        val storageDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                .toString() + "/Sedate_Farmer"
+        )
+        var success = true
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs()
+        }
+        if (success) {
+            val imageFile = File(storageDir, imageFileName)
+            savedImagePath = imageFile.getAbsolutePath()
+            try {
+                val fOut: OutputStream = FileOutputStream(imageFile)
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
+                fOut.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            galleryAddPic(savedImagePath)}
+        return savedImagePath
+    }
+
+    private fun galleryAddPic(imagePath: String?) {
+        imagePath?.let { path ->
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            val f = File(path)
+            val contentUri: Uri = Uri.fromFile(f)
+            mediaScanIntent.data = contentUri
+            requireActivity().sendBroadcast(mediaScanIntent)
         }
     }
 }
